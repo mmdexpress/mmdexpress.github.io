e6a05105c9a4711b2052c6a2ba8f403a10ded671
Wei Tat Lee <weitat95@live.com>
2020-06-17 17:16:13 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -9,24 +9,24 @@
       });
     });
 
-// /* 2. sticky And Scroll UP */
-//     $(window).on('scroll', function () {
-//       var scroll = $(window).scrollTop();
-//       if (scroll < 400) {
-//         $(".header-sticky").removeClass("sticky-bar");
-//         $('#back-top').fadeOut(500);
-//       } else {
-//         $(".header-sticky").addClass("sticky-bar");
-//         $('#back-top').fadeIn(500);
-//       }
-//     });
-//   // Scroll Up
-//     $('#back-top a').on("click", function () {
-//       $('body,html').animate({
-//         scrollTop: 0
-//       }, 800);
-//       return false;
-//     });
+/* 2. sticky And Scroll UP */
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 400) {
+        $(".header-sticky").removeClass("sticky-bar");
+        $('#back-top').fadeOut(500);
+      } else {
+        $(".header-sticky").addClass("sticky-bar");
+        $('#back-top').fadeIn(500);
+      }
+    });
+  // Scroll Up
+    $('#back-top a').on("click", function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
   
 
 /* 3. slick Nav */
